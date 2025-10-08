754a4ef98582bf664dd466a5486bcbab5fceedaf
Paul Kimball <pekimball@interfaceassociates.net>
2025-10-08 10:36:31 -0400

    Initial Commit - there are two files with main methods

--- src/main/java/com/pluralsight/files/MainWithResources.java
+++ src/main/java/com/pluralsight/files/MainWithResources.java
@@ -1,8 +1,6 @@
 package com.pluralsight.files;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 
 public class MainWithResources {
@@ -10,17 +8,19 @@ public class MainWithResources {
 
 
         // try with resources
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("myfile.txt"))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("myfile.txt"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"))) {
 
             String input;
             while ((input = bufferedReader.readLine()) != null) {
-                System.out.println(input);
+                bufferedWriter.write(input);
             }
 
         }
         catch (IOException e){
-            System.out.println("Problem reading file");
+            System.out.println("Problem reading or writing file");
         }
         // Streams and files are closed automatically
+
     }
 }
